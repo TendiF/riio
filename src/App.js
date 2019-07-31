7b0037a7c5801d3ef3191cc2491529c3f0371b31
Tendi F <tendifirmansyah30@gmail.com>
2019-07-31 10:17:42 +0700

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css"
 
-function App() {
+import Home from "./view/Home"
+import Topics from "./view/Topics"
+
+function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default BasicExample;
