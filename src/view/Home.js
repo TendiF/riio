14ccd0d0265d79f5e33ba43f9d2e7ad41ad0b823
Tendi F <tendifirmansyah30@gmail.com>
2019-07-31 15:22:53 +0700

    install and try eslint

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router,Route,Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 function Home() {
-    return (
-      <div>
-        <h2>Home</h2>
-        <Link to="/topics">Topics</Link>
-      </div>
-    );
+  return (
+    <div>
+      <h2>Home</h2>
+      <Link to="/topics">Topics</Link>
+    </div>
+  );
 }
 export default Home;
